ad381c92fbd0e7c78b8002df822cf8d95e291207
mcmillanator <8310830+mcmillanator@users.noreply.github.com>
2025-03-26 10:54:41 -0400

    add feature test for create-remote-user

---
+++ test/create-remote-user/create-remote-user.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+# Optional: Import test library bundled with the devcontainer CLI
+source dev-container-features-test-lib
+
+# Feature-specific tests
+# The 'check' command comes from the dev-container-features-test-lib.
+check "scenario create-remote-user created the user" bash -c "getent passwd devuser | grep 'devuser'"
+
+# Report results
+# If any of the checks above exited with a non-zero exit code, the test will fail.
+reportResults
